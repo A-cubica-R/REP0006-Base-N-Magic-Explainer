76dfbd66e9a8b70a2616d769961267290ce70c63
a-cubica-r <adolfoalejandroarenasramos@hotmail.com>
2025-05-09 21:57:09 -0500

    refactoring the code and the structure

--- code/converser_init.asm
+++ code/converser_init.asm
@@ -2,34 +2,25 @@
 .stack 100h
 
 .data
-
-    ; ---------------------------------------------------
-    ; Títulos Style A
-    ; ---------------------------------------------------
+    ; Title Style A
     title1a     db "  ############################  ",13,10,"$"
     title2a     db "  #   UNIT CONVERTER 8-BIT   #  ",13,10,"$"
     title3a     db "  #       Base-N Magic!      #  ",13,10,"$"
     title4a     db "  ############################  ",13,10,"$"
 
-    ; ---------------------------------------------------
-    ; Títulos Style B
-    ; ---------------------------------------------------
+    ; Title Style B
     title1b     db "  ****************************  ",13,10,"$"
     title2b     db "  *   UNIT CONVERTER 8-BIT   *  ",13,10,"$"
     title3b     db "  *       Base-N Magic!      *  ",13,10,"$"
     title4b     db "  ****************************  ",13,10,"$"
 
-    ; ---------------------------------------------------
     ; Títulos Style C
-    ; ---------------------------------------------------
     title1c     db "  ============================  ",13,10,"$"
     title2c     db "  |   UNIT CONVERTER 8-BIT   |  ",13,10,"$"
     title3c     db "  |       Base-N Magic!      |  ",13,10,"$"
     title4c     db "  ============================  ",13,10,"$"
 
-    ; ---------------------------------------------------
     ; Menús Style A
-    ; ---------------------------------------------------
     menu1a      db "  ############################  ",13,10,"$"
     menu2a      db "  #   [A] ABOUT              #  ",13,10,"$"
     menu3a      db "  #   [C] CONVERT            #  ",13,10,"$"
@@ -37,9 +28,7 @@
     menu5a      db "  #  Enter an option (MAYUS) #  ",13,10,"$"
     menu6a      db "  ############################  ",13,10,"$"
 
-    ; ---------------------------------------------------
     ; Menús Style B
-    ; ---------------------------------------------------
     menu1b      db "  ****************************  ",13,10,"$"
     menu2b      db "  *   [A] ABOUT              *  ",13,10,"$"
     menu3b      db "  *   [C] CONVERT            *  ",13,10,"$"
@@ -47,9 +36,7 @@
     menu5b      db "  *  Enter an option (MAYUS) *  ",13,10,"$"
     menu6b      db "  ****************************  ",13,10,"$"
 
-    ; ---------------------------------------------------
     ; Menús Style C
-    ; ---------------------------------------------------
     menu1c      db "  ============================  ",13,10,"$"
     menu2c      db "  |   [A] ABOUT              |  ",13,10,"$"
     menu3c      db "  |   [C] CONVERT            |  ",13,10,"$"
@@ -57,19 +44,13 @@
     menu5c      db "  |  Enter an option (MAYUS) |  ",13,10,"$"
     menu6c      db "  ============================  ",13,10,"$"
 
-    ; ---------------------------------------------------
     ; Dynamic line
-    ; ---------------------------------------------------
     selectedMsg db "Char ' ' selected, press ENTER to continue:",13,10,"$"
 
-    ; ---------------------------------------------------
     ; Prompt message
-    ; ---------------------------------------------------
     prompt      db "Select your choice: $"
 
-    ; ---------------------------------------------------
     ; Buffer for the selected character
-    ; ---------------------------------------------------
     charBuffer  db 0
 
 .code
@@ -78,131 +59,24 @@
                         mov  ds, ax
 
     menu_animation_loop:
-    ; === Style A ===
-                        call ClearScreen
-
-                        lea  dx, title1a
-                        call PrintString
-                        lea  dx, title2a
-                        call PrintString
-                        lea  dx, title3a
-                        call PrintString
-                        lea  dx, title4a
-                        call PrintString
-
-                        lea  dx, menu1a
-                        call PrintString
-                        lea  dx, menu2a
-                        call PrintString
-                        lea  dx, menu3a
-                        call PrintString
-                        lea  dx, menu4a
-                        call PrintString
-                        lea  dx, menu5a
-                        call PrintString
-                        lea  dx, menu6a
-                        call PrintString
-
-    ; Dinamic line
-    ; "Char 'X' selected..."
-                        lea  dx, selectedMsg
-                        call PrintString
-
-    ; prompt
-                        lea  dx, prompt
-                        call PrintString
-
-                        call Delay
-                        call CheckKey
-
-    ; === Style B ===
-                        call ClearScreen
-
-                        lea  dx, title1b
-                        call PrintString
-                        lea  dx, title2b
-                        call PrintString
-                        lea  dx, title3b
-                        call PrintString
-                        lea  dx, title4b
-                        call PrintString
-
-                        lea  dx, menu1b
-                        call PrintString
-                        lea  dx, menu2b
-                        call PrintString
-                        lea  dx, menu3b
-                        call PrintString
-                        lea  dx, menu4b
-                        call PrintString
-                        lea  dx, menu5b
-                        call PrintString
-                        lea  dx, menu6b
-                        call PrintString
-
-    ; Dinamic line
-                        lea  dx, selectedMsg
-                        call PrintString
-
-    ; prompt
-                        lea  dx, prompt
-                        call PrintString
-
-                        call Delay
-                        call CheckKey
-
-    ; === Style C ===
-                        call ClearScreen
-
-                        lea  dx, title1c
-                        call PrintString
-                        lea  dx, title2c
-                        call PrintString
-                        lea  dx, title3c
-                        call PrintString
-                        lea  dx, title4c
-                        call PrintString
-
-                        lea  dx, menu1c
-                        call PrintString
-                        lea  dx, menu2c
-                        call PrintString
-                        lea  dx, menu3c
-                        call PrintString
-                        lea  dx, menu4c
-                        call PrintString
-                        lea  dx, menu5c
-                        call PrintString
-                        lea  dx, menu6c
-                        call PrintString
-
-    ; Dinamic line
-                        lea  dx, selectedMsg
-                        call PrintString
-
-    ; prompt
-                        lea  dx, prompt
-                        call PrintString
-
-                        call Delay
-                        call CheckKey
-
+    
+                        call ShowWelcomeVariantA
+                        call ShowWelcomeVariantB
+                        call ShowWelcomeVariantC
                         jmp  menu_animation_loop           ; infinitely loop
 
-    ; ---------------------------------------------------
+    ; ####################################################
     ; Subroutines
-    ; ---------------------------------------------------
+    ; ####################################################
     PrintString:        
                         mov  ah, 09h
                         int  21h
                         ret
-
     ClearScreen:        
                         mov  ah, 0
                         mov  al, 3
                         int  10h
                         ret
-
     Delay:              
                         push cx
                         mov  cx, 0FFFFh
@@ -215,30 +89,127 @@
                         loop DelayLoop2
                         pop  cx
                         ret
+    ; ===================================================
+    ; Show the welcome variant A of the animation illution
+    ShowWelcomeVariantA:
+    ; ---------------------------------------------------
+    ; Menu animation - Variant A
+    ; ---------------------------------------------------
+                        call ClearScreen                   ; clear screen
+                        lea  dx, title1a                   ; defining the title A
+                        call PrintString
+                        lea  dx, title2a
+                        call PrintString
+                        lea  dx, title3a
+                        call PrintString
+                        lea  dx, title4a
+                        call PrintString
+                        lea  dx, menu1a                    ; defining the menu A
+                        call PrintString
+                        lea  dx, menu2a
+                        call PrintString
+                        lea  dx, menu3a
+                        call PrintString
+                        lea  dx, menu4a
+                        call PrintString
+                        lea  dx, menu5a
+                        call PrintString
+                        lea  dx, menu6a
+                        call PrintString
+                        lea  dx, selectedMsg               ; Dinamic line for selected character
+                        call PrintString
+                        lea  dx, prompt                    ; write line for the user
+                        call PrintString
+                        call Delay                         ; delay for a while
+                        call CheckKey                      ; check if the correct key was pressed
 
+    ; ===================================================
+    ; Show the welcome variant A of the animation illution
+    ShowWelcomeVariantB:
+    ; ---------------------------------------------------
+    ; Menu animation - Variant A
+    ; ---------------------------------------------------
+                        call ClearScreen                   ; clear screen
+                        lea  dx, title1b                   ; defining the title B
+                        call PrintString
+                        lea  dx, title2b
+                        call PrintString
+                        lea  dx, title3b
+                        call PrintString
+                        lea  dx, title4b
+                        call PrintString
+                        lea  dx, menu1b                    ; defining the menu B
+                        call PrintString
+                        lea  dx, menu2b
+                        call PrintString
+                        lea  dx, menu3b
+                        call PrintString
+                        lea  dx, menu4b
+                        call PrintString
+                        lea  dx, menu5b
+                        call PrintString
+                        lea  dx, menu6b
+                        call PrintString
+                        lea  dx, selectedMsg               ; Dinamic line for selected character
+                        call PrintString
+                        lea  dx, prompt                    ; write line for the user
+                        call PrintString
+                        call Delay                         ; delay for a while
+                        call CheckKey                      ; check if the correct key was pressed
+
+    ; ===================================================
+    ; Show the welcome variant A of the animation illution
+    ShowWelcomeVariantC:
+    ; ---------------------------------------------------
+    ; Menu animation - Variant A
+    ; ---------------------------------------------------
+                        call ClearScreen                   ; clear screen
+                        lea  dx, title1c                   ; defining the title C
+                        call PrintString
+                        lea  dx, title2c
+                        call PrintString
+                        lea  dx, title3c
+                        call PrintString
+                        lea  dx, title4c
+                        call PrintString
+                        lea  dx, menu1c                    ; defining the menu C
+                        call PrintString
+                        lea  dx, menu2c
+                        call PrintString
+                        lea  dx, menu3c
+                        call PrintString
+                        lea  dx, menu4c
+                        call PrintString
+                        lea  dx, menu5c
+                        call PrintString
+                        lea  dx, menu6c
+                        call PrintString
+                        lea  dx, selectedMsg               ; Dinamic line for selected character
+                        call PrintString
+                        lea  dx, prompt                    ; write line for the user
+                        call PrintString
+                        call Delay                         ; delay for a while
+                        call CheckKey                      ; check if the correct key was pressed
+
+    ; ===================================================
+    ; Check if the key pressed is A or C, if not, restart the animation
+    CheckKey:           
     ; ---------------------------------------------------
     ; CheckKey:
-    ;  - Only accepts A or C.
-    ;  - On ENTER, validates the buffer:
-    ;      * if it's A/C → prints selectedMsg + exits.
-    ;      * if not      → clears the screen and restarts the animation.
-    ;  - When storing A/C, updates selectedMsg[6].
+    ;  * if it's A/C → prints selectedMsg + exits.
+    ;  * if not      → clears the screen and restarts the animation.
     ; ---------------------------------------------------
-    CheckKey:           
                         mov  ah, 0Bh                       ; test keyboard without waiting
                         int  21h
                         cmp  al, 0
                         je   .no_key
-
                         mov  ah, 01h                       ; read character (blocking if buffer)
                         int  21h                           ; AL = key
-
                         cmp  al, 'A'
                         je   .store_char
                         cmp  al, 'C'
                         je   .store_char
-    ; not A/C → ignore
-                        jmp  .no_key
+                        jmp  .no_key                       ; not A/C → ignore
 
 .store_char:
                         mov  [charBuffer], al
